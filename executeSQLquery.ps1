464552889e003f541d1c0a1d9b009a0095060ac5
masterlexa <47365555+masterlexa@users.noreply.github.com>
2019-06-23 16:48:00 +0300

    Publication

---
+++ executeSQLquery.ps1
@@ -0,0 +1,15 @@
+Get-Content $args[0] | Foreach-Object{ # подгружаем конфиг из 1 параметра
+   $var = $_.Split('=')
+   New-Variable -Name $var[0] -Value $var[1]
+}
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection # Создаем объект соединения 
+$SqlConnection.ConnectionString = "Server=$serverName; Database=$instanceName; User ID=$userName; Password=$userPwd;" # Формируем строку подключения
+$SqlConnection.Open() # Открываем соединение
+$SqlCmd = $SqlConnection.CreateCommand() # Создаем запрос
+$SqlCmd.CommandText = Get-Content $args[1] # Получаем текст запроса из файла 2 параметра
+$objReader = $SqlCmd.ExecuteReader() # Выполняем запрос
+while ($objReader.read()) {
+  echo $objReader.GetValue(0) # Выводим результат запроса
+}
+$objReader.close() # Закрываем Reader результата
+$SqlConnection.close() # Закрываем соединение
